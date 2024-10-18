b057212657df66dc20fc1e455d407efba7f75239
TugçeBuse <tugcebuse7@gmail.com>
2024-10-18 17:35:34 +0300

    Navbar
    değiştirildi.

---
+++ bgf-uygulamasi1/src/Homepage/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import "./navbar.css"
+
+const Navbar = () =>
+{
+    return (
+    
+    <div> 
+        <nav className="navbar">
+         
+             <img
+                  className="logo"
+                  src="/logo.png"
+                  alt="Farkindalik Uygulamasi"
+        ></img>
+         <h1 className="h1">SafeClicks</h1>
+      
+       
+        </nav>
+    </div>
+
+    );
+}
+export default Navbar;
