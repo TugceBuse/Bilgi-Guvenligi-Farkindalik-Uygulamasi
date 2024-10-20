888e7f383e32202798090fd6ca7c2cecbe3cb56b
Ahmet Karaköse <ahmet_krks1@hotmail.com>
2024-10-20 20:57:13 +0300

    feat:add mailbox styles with dumb examples

--- bgf-uygulamasi1/src/Desktop/Container/Container.jsx
+++ bgf-uygulamasi1/src/Desktop/Container/Container.jsx
@@ -4,12 +4,26 @@ import { useState } from 'react';
 
 const Container = () => {
 
+  //recycleBin penceresini açmak için state tanımladık
   const [openWindow, setOpenWindow] = useState(null);
-
-
   const handleIconClick = (windowName) => {
     setOpenWindow(windowName);
   }
+  //////////////////////////////////////////////
+
+  //Mail kutusu açma işlemleri
+  const [isMailboxOpen, setIsMailboxOpen] = useState(false);
+
+  const openMailbox = () => {
+    setIsMailboxOpen(true);
+  };
+
+  const closeMailbox = () => {
+    setIsMailboxOpen(false);
+  };
+  //////////////////////////////////////////////
+
+  
 
   return (
 
@@ -22,7 +36,7 @@ const Container = () => {
             <span>My Documents</span>
           </div>
 
-          <div className="icon">
+          <div className="icon" onClick={openMailbox}>
             <img src="/icons/mail.png" alt="Mail Icon" />
             <span>Mail</span>
           </div>
@@ -45,6 +59,60 @@ const Container = () => {
           </div>    
         </div>
       )}
+
+      {/* Mailbox penceresi */}
+      {isMailboxOpen && (
+      <div className="mailbox-window">
+        
+          <div className="mailbox-header">
+            <h2>Mailbox</h2>
+              <button className="mailbox-close" onClick={closeMailbox}>×</button>
+          </div>
+          {/* headerden sonra soldan sağa dizilen iç pencere */}
+        <div className="mailbox-inwindow">
+
+          <div className="mailbox-menu">
+            <ul>
+              <li className="active">Inbox</li>
+              <li>Sent</li>
+            </ul>
+          </div>
+          {/* vvvvvvvvvvvvv mail listesi vvvvvvvvvvvvv
+           - Mailler dinamik eklenebilir hale gelmeli
+           - bir maile tıklandığında içeriği mailbox-mailcontent e gelmeli
+         */}
+          <div className="mailbox-mails">
+            <ul className="mailbox-maillist">
+              <li className="active">
+                <h3>Mail1</h3>
+                <p>Mail1 content</p>
+              </li>
+              <li>
+                <h3>Mail2</h3>
+                <p>Mail2 content</p>
+              </li>
+              <li>
+                <h3>Mail3</h3>
+                <p>Mail3 content</p>
+              </li>
+            </ul>
+          </div>
+
+            {/* mail içeriği --> Bu kısma Header falan eklenerek
+            mailin kimden ne zaman geldiği tarzında bilgiler eklenmeli */}
+            <div className="mailbox-mailcontent">
+              
+            </div>
+
+        </div>
+          
+        
+      </div>
+
+      )}
+
+
+
     </div>
   );
 };
