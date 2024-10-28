ce88277083ae49979b80bac608e0da44a4841be8
Ahmet Karaköse <ahmet_krks1@hotmail.com>
2024-10-29 00:35:42 +0300

    feat: add loading animation in browser

--- bgf-uygulamasi1/src/Game/Browser/Browser.jsx
+++ bgf-uygulamasi1/src/Game/Browser/Browser.jsx
@@ -17,20 +17,33 @@ export const useBrowser = () => {
 
 
 const Browser = ({ closeBrowser }) => {
+
   const [url, setUrl] = useState('');
   const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(false);
+  
 
   const handleUrlChange = (e) => {
     setUrl(e.target.value);
   };
 
   const handleGoClick = () => {
+    setLoading(true);
+    setTimeout(() => {
     if (url === '192.168.1.1') {
       setContent('Example Content:This is the content for example.com.');
     } else if (url === 'https://another.com') {
       setContent('Another Content:This is the content for another.com.');
     } else {
       setContent('404 Not Found.The requested URL was not found on this server.');
+    }
+    setLoading(false);
+    }, 2000); // 2 saniye gecikme
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleGoClick();
     }
   };
 
@@ -41,14 +54,35 @@ const Browser = ({ closeBrowser }) => {
           type="text"
           value={url}
           onChange={handleUrlChange}
+          onKeyDown={handleKeyDown}
           placeholder="Enter URL"
           className="browser-url-input"
         />
         <button onClick={handleGoClick} className="browser-go-button">Go</button>
         <button className="browser-close" onClick={closeBrowser}>×</button>      
       </div>
-      <div className="browser-content"/>
-        <p>{content}</p> 
+      <div className="browser-content">
+      {loading ? (
+          <div className="browser-loading">
+            <div className="lds-default">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        ) : (
+          <div>{content}</div>
+        )}
+      </div>
     </div>
   );
 };
