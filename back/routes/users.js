ddb8bf020970437b2638c5e13758f9085251cfe0
Ahmet Karaköse <ahmet_krks1@hotmail.com>
2024-12-30 23:33:23 +0300

    fix(users.js):fix authMiddleware import!

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/authMiddleware');
+
+
 const {
   registerUser,
   deleteUser,
