4174a8a2fa78991c2de6f9cdd10554627ceb857c
TugçeBuse <tugcebuse7@gmail.com>
2025-02-05 13:43:59 +0300

    fix: fixed start simulation button in HomePage

--- front/src/pages/Homepage/HomeContainer/HomeContainer.jsx
+++ front/src/pages/Homepage/HomeContainer/HomeContainer.jsx
@@ -4,38 +4,28 @@ import "./homeContainer.css";
 
 const HomeContainer = () => {
   return (
-   <div className="moving-background">
-        
+      <div className="moving-background">
+        {/* Bilgilendirme Kısmı */}
         <div className="FirstPart">
-            <div className="circle">
-            <div className="line">
-            
+            <div className="circle"></div>
+            <div className="line"></div>
+
+          <div style={{display:"flex", flexDirection:"column"}}>
+            <h1>Hoş Geldiniz!</h1>
+            <h3>Bu simülasyon, bilgi güvenliği farkındalığınızı artırmak, </h3> 
+
+            <div style={{display:"flex", flexDirection:"column"}}>
+              <h3>olası siber saldırılara karşı sizi hazırlamak amacıyla tasarlanmıştır. </h3>
+              <h3>Çalışma ortamında karşılaşabileceğiniz tehditler ve </h3>
+              <h3>hatalarla ilgili çeşitli senaryolarla kendinizi test edebilir, doğru güvenlik alışkanlıkları kazanabilirsiniz.</h3>
+              <h4>Başlamak için aşağıdaki “Simülasyonu Başlat” butonuna tıklayınız.</h4>
             </div>
-              
-            </div>
-                    <div style={{display:"flex", flexDirection:"column"}}>
-                          <h1>Hoş Geldiniz!</h1>
-                          <h3>Bu simülasyon, bilgi güvenliği farkındalığınızı artırmak, </h3> 
-
-                          <div style={{display:"flex", flexDirection:"column"}}>
-                          <h3>olası siber saldırılara karşı sizi hazırlamak amacıyla tasarlanmıştır. </h3>
-                          <h3>Çalışma ortamında karşılaşabileceğiniz tehditler ve </h3>
-                          <h3>hatalarla ilgili çeşitli senaryolarla kendinizi test edebilir, doğru güvenlik alışkanlıkları kazanabilirsiniz.</h3>
-                          <h4>Başlamak için aşağıdaki “Simülasyonu Başlat” butonuna tıklayınız.</h4>
-                          </div>
-                    </div>
-
-        </div>
-
-
-       
-        
-         
-     
-      <WorkSpaceModel />
+          </div>
+      </div>
       
+      {/* Model */}
+      <WorkSpaceModel />
    </div>
-   
   );
 };
 
